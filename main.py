8e00ccb0310789f5ee022353851f9d09a7c11958
vishalmishra2495 <vishalmishra2495@gmail.com>
2020-04-21 04:38:02 -0400

    Third times the charm

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ import time
 import schedule
 from apscheduler.scheduler import Scheduler
 import numpy as np
-import plotly.express as px
-import matplotlib.pyplot as plt
+
+
 import atexit
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
+from pymongo import MongoClient
 import pickle
 
 
@@ -33,6 +34,24 @@ os.environ['KAGGLE_KEY'] = "d4f77b92179bfa82065bccfaa9c1dc54"
 os.system("kaggle datasets download -d sudalairajkumar/novel-corona-virus-2019-dataset")
 zf = zipfile.ZipFile('novel-corona-virus-2019-dataset.zip')
 df = pd.read_csv(zf.open('covid_19_data.csv'))
+client = MongoClient("mongodb+srv://m220-student:m220-mongodb-basics@mflix-sa05a.mongodb.net/DataProgrammingTestProject?retryWrites=true&w=majority")
+collection=client["DataProgrammingTestProject"]
+db = collection["NewCovidData2020"]
+db.drop()
+records_ = df.to_dict(orient = "records")
+result = db.insert_many(records_)
+x=db.count_documents({})
+print("number of records",x)
+headData = db.find()
+row_list = []
+for i in headData:
+    row_list.append(i)
+
+
+df_database=pd.DataFrame(row_list)
+
+# df.drop("_id",axis=1,inplace=True)
+
 df['ObservationDate']=pd.to_datetime(df['ObservationDate'])
 df_grouped=df.groupby(['ObservationDate','Country/Region']).sum()
 
@@ -40,34 +59,49 @@ df_grouped=df.groupby(['ObservationDate','Country/Region']).sum()
 df_new=df_grouped.xs(df['ObservationDate'].max())
 
 data= df.groupby(["ObservationDate"])['Confirmed','Deaths', 'Recovered'].sum().reset_index()
+print(data.tail())
 x_data=pd.DataFrame(data.index)
 y_data=pd.DataFrame(data.Confirmed)
-print(data.head())
-# print(x_data.head())
-# print(y_data.head())
-# poly=PolynomialFeatures(degree=9)
-# x_train,x_test,y_train,y_test=train_test_split(x_data,y_data,test_size=0.33,random_state=10)
-# poly=PolynomialFeatures(degree=9)
-# x_data=poly.fit_transform(x_data)
-# lm=LinearRegression()
-# lm.fit(x_data,y_data)
-# lm.score(x_train,y_train)
-# predicted=lm.predict(poly.fit_transform(x_test))
-# error=np.sqrt(mean_squared_error(y_test,predicted))
-# print(error)
-# print(lm.score(poly.fit_transform(x_test),y_test))
-# print(lm.predict(poly.fit_transform([[68]])))
-# filename="finalized_model.pickle"
-# filename_2="poly.pickle"
-# pickle.dump(poly,open(filename_2,"wb"))
-# pickle.dump(lm,open(filename,"wb"))
-# model=pickle.load(open("finalized_model.pickle","rb"))
-# poly_loaded=pickle.load(open("poly.pickle","rb"))
-# model=pickle.load(open("finalized_model.pickle","rb"))
-# poly_loaded=pickle.load(open("poly.pickle","rb"))
-print(len(data))
-print("THis is on branch Pankaj")
-print("This is also on branch Pankaj")
+x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.33, random_state=10)
+rmses = []
+degrees = np.arange(1, 15)
+min_rmse, min_deg = 1e10, 0
+for deg in degrees:
+
+    poly_features = PolynomialFeatures(degree=deg,include_bias=False)
+    x_poly_train = poly_features.fit_transform(x_train)
+
+    poly_reg = LinearRegression()
+    poly_reg.fit(x_poly_train, y_train)
+
+    x_poly_test = poly_features.fit_transform(x_test)
+    poly_predict = poly_reg.predict(x_poly_test)
+    poly_mse = mean_squared_error(y_test, poly_predict)
+    poly_rmse = np.sqrt(poly_mse)
+    rmses.append(poly_rmse)
+
+    if min_rmse > poly_rmse:
+        min_rmse = poly_rmse
+        min_deg = deg
+
+print('Best degree {} with RMSE {}'.format(min_deg, min_rmse))
+poly = PolynomialFeatures(degree=min_deg)
+x_transformed_data = poly.fit_transform(x_data)
+poly_reg = LinearRegression()
+poly_reg.fit(x_transformed_data, y_data)
+poly_reg.predict((poly.fit_transform([[len(data) - 1]])))
+filename = "finalized_model.pickle"
+filename_2 = "poly.pickle"
+pickle.dump(poly, open(filename_2, "wb"))
+pickle.dump(poly_reg, open(filename, "wb"))
+trial = len(data)
+print(trial)
+model = pickle.load(open("finalized_model.pickle", "rb"))
+poly_loaded = pickle.load(open("poly.pickle", "rb"))
+print("First time lets see", model.predict(poly_loaded.fit_transform([[trial-1]])))
+print("lets see")
+# pickle.dump(lm, open(filename, "wb"))
+print(data.tail())
 
 
 
@@ -82,12 +116,34 @@ print("This is also on branch Pankaj")
 
 
 
-@cron.interval_schedule(minutes=10)
+
+
+
+@cron.interval_schedule(minutes=600)
+
+
 def get_data():
-
     os.system("kaggle datasets download -d sudalairajkumar/novel-corona-virus-2019-dataset")
     zf = zipfile.ZipFile('novel-corona-virus-2019-dataset.zip')
+
     df_updated = pd.read_csv(zf.open('covid_19_data.csv'))
+    client_new = MongoClient("mongodb+srv://m220-student:m220-mongodb-basics@mflix-sa05a.mongodb.net/DataProgrammingTestProject?retryWrites=true&w=majority")
+    collection_new = client_new["DataProgrammingTestProject"]
+    db_new = collection_new["NewCovidData2020"]
+    db_new.drop()
+    records_new_ = df_updated.to_dict(orient="records")
+    result_new = db.insert_many(records_new_)
+    x_new = db.count_documents({})
+    print("number of records", x_new)
+    headData_new = db.find()
+    row_list_new = []
+    for i in headData_new:
+        row_list_new.append(i)
+
+    df_updated_database=pd.DataFrame(row_list_new)
+
+    df_updated_database.drop("_id", axis=1, inplace=True)
+
     df.update(df_updated)
     df['ObservationDate'] = pd.to_datetime(df['ObservationDate'])
     df_grouped_updated = df.groupby(['ObservationDate', 'Country/Region']).sum()
@@ -98,20 +154,83 @@ def get_data():
     data = df.groupby(["ObservationDate"])['Confirmed', 'Deaths', 'Recovered'].sum().reset_index()
     x_data = pd.DataFrame(data.index)
     y_data = pd.DataFrame(data.Confirmed)
-    poly = pickle.load(open("poly.pickle", "rb"))
-    lm = LinearRegression()
+
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.33, random_state=10)
+    rmses = []
+    degrees = np.arange(1, 15)
+    min_rmse, min_deg = 1e10, 0
+
+    for deg in degrees:
+
+        poly_features = PolynomialFeatures(degree=deg, include_bias=False)
+        x_poly_train = poly_features.fit_transform(x_train)
+
+        poly_reg = LinearRegression()
+        poly_reg.fit(x_poly_train, y_train)
+
+        x_poly_test = poly_features.fit_transform(x_test)
+        poly_predict = poly_reg.predict(x_poly_test)
+        poly_mse = mean_squared_error(y_test, poly_predict)
+        poly_rmse = np.sqrt(poly_mse)
+        rmses.append(poly_rmse)
+
+        if min_rmse > poly_rmse:
+            min_rmse = poly_rmse
+            min_deg = deg
+
+    print('Best degree {} with RMSE {}'.format(min_deg, min_rmse))
+    poly = PolynomialFeatures(degree=min_deg)
     x_data = poly.fit_transform(x_data)
-
-
-    lm.fit(x_data, y_data)
-    trial=len(data)
-    print("Lets get this party started",lm.predict(poly.fit_transform([[trial+1]])))
+    poly_reg = LinearRegression()
+    poly_reg.fit(x_data, y_data)
+    poly_reg.predict((poly.fit_transform([[len(data) - 1]])))
     filename = "finalized_model.pickle"
-    pickle.dump(lm, open(filename, "wb"))
+    filename_2 = "poly.pickle"
+    pickle.dump(poly, open(filename_2, "wb"))
+    pickle.dump(poly_reg, open(filename, "wb"))
+    model = pickle.load(open("finalized_model.pickle", "rb"))
+    poly_loaded = pickle.load(open("poly.pickle", "rb"))
+
+    trial = len(data)
+    print(trial)
+    print("Lets get this party started with mongodb", model.predict(poly_loaded.fit_transform([[trial]])))
+    filename = "finalized_model.pickle"
+    # pickle.dump(lm, open(filename, "wb"))
     print(data.tail())
 
     print("file updated")
-@app.route('/')
+@app.route('/',methods=['POST','GET'])
+def imp():
+    if request.method =='POST':
+        try:
+            day=float(request.form['day'])
+            filename = "finalized_model.pickle"
+            load_model = pickle.load(open(filename, 'rb'))
+            poly_loaded = pickle.load(open("poly.pickle", "rb"))
+            prediction = load_model.predict(poly_loaded.fit_transform([[day]]))
+            print("Inside the html",prediction[0][0])
+            num=x_data.tail(5).values.tolist()
+            cases=y_data.tail(5).values.tolist()
+            print("Inside predictor",num[0][0])
+            schedule.every(600).minutes.do(get_data)
+
+
+            return render_template("result.html",prediction=round(prediction[0][0]),num_cases=zip(num,cases))
+        except Exception as e:
+            print('The Exception message is: ', e)
+            return 'something is wrong'
+
+
+    else:
+
+        schedule.every(600).minutes.do(get_data)
+        return render_template("forecast.html")
+
+
+
+
+
+@app.route('/map')
 def first():
 
 
@@ -119,6 +238,7 @@ def first():
     #     go.Bar(x=df['Country/Region'],y=df['Confirmed'])
     #
     # ]
+    df_new['text']='<br>'+'Confirmed :' + df_new['Confirmed'].astype(str) + '<br>'+'Recovered :' + df_new['Recovered'].astype(str) + '<br>' +'Deaths :' + df_new['Deaths'].astype(str)
     fig = go.Choropleth(
         locations=df_new.index,  # Spatial coordinates
         z=df_new['Confirmed'],  # Data to be color-coded
@@ -126,9 +246,9 @@ def first():
         colorscale='Blues',
         showlegend=False,
 
-        text=df_new['Deaths'],
+        text=df_new['text'],
         hovertext=df_new.index,
-        hovertemplate="<b>Confirmed<b>:%{z},Death:%{text},Country:%{hovertext}",
+        hovertemplate="Country:%{hovertext},%{text}",
         colorbar_title='Confirmed<br>Cases',
 
 
@@ -139,7 +259,7 @@ def first():
     graphJSON_1 = json.dumps(d4, cls=plotly.utils.PlotlyJSONEncoder)
 
 
-    schedule.every(1).minutes.do(get_data)
+    schedule.every(600).minutes.do(get_data)
     return render_template('index.html',
                            graphJSON=graphJSON_1)
 
@@ -157,7 +277,7 @@ def second():
     # d2 = [fig]
     # graphJSON_3 = json.dumps(d2, cls=plotly.utils.PlotlyJSONEncoder)
 
-    schedule.every(1).minutes.do(get_data)
+    schedule.every(600).minutes.do(get_data)
     return render_template('index_2.html',
                            graphJSON=graphJSON_1)
 
@@ -169,8 +289,7 @@ def third():
     d1 = [fig]
     graphJSON_2 = json.dumps(d1, cls=plotly.utils.PlotlyJSONEncoder)
 
-
-    schedule.every(1).minutes.do(get_data)
+    schedule.every(600).minutes.do(get_data)
     return render_template('index_3.html',
                            graphJSON=graphJSON_2)
 @app.route('/scatter_3')
@@ -178,8 +297,8 @@ def fourth():
     fig = go.Scatter(y=data['Deaths'], x=data['ObservationDate'])
     d1 = [fig]
     graphJSON_2 = json.dumps(d1, cls=plotly.utils.PlotlyJSONEncoder)
-    schedule.every(1).minutes.do(get_data)
-    return render_template('index_3.html',
+    schedule.every(600).minutes.do(get_data)
+    return render_template('index_4.html',
                            graphJSON=graphJSON_2)
 
 
@@ -188,24 +307,7 @@ def fourth():
 
 
 
-@app.route('/predict',methods=['POST','GET'])
-def imp():
-    if request.method =='POST':
-        try:
-            day=float(request.form['day'])
-            filename = "finalized_model.pickle"
-            load_model = pickle.load(open(filename, 'rb'))
-            poly_loaded = pickle.load(open("poly.pickle", "rb"))
-            prediction = load_model.predict(poly_loaded.fit_transform([[day]]))
-            print("Inside the html",prediction[0][0])
-            return render_template("result.html",prediction=round(prediction[0][0]))
-        except Exception as e:
-            print('The Exception message is: ', e)
-            return 'something is wrong'
 
-
-    else:
-        return render_template("forecast.html")
 
 
 
@@ -216,10 +318,6 @@ def imp():
 
 if __name__=='__main__':
     app.run(debug=True)
-
-
-
-
 
 
 
